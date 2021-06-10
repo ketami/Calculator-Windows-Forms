38c54cd66a60a6c3dd5fd7a3ff044e2b3af2099a
Никита Мелков <linokran@yandex.ru>
2021-06-10 06:04:06 +0300

    Создание интерфейса

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -11,11 +11,34 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
+
+    public delegate double OperationMethod(double a, double b);
+    public class OperationPanel : Panel
+    {
+        public static int PanelCounter = -1;
+        public int X_Elements { get; set; }
+        public int Y_Elements { get; set; }
+        public void AddOperation(string OperationName, OperationMethod operationMethod)
+        {
+            this.Controls.Add(new OperationButton(OperationName, operationMethod));
+
+        }
+        private void OperationButton_Setter(Button newButton, byte PanelCounter)
+        {
+            int x = Width * PanelCounter + Width * ((((OperationButton)newButton).Index - 1) % 2) / 2 + Width / 16;
+            int y = -Height * PanelCounter + Height * ((((OperationButton)newButton).Index - 1) / 2) / 6 + Height / 25;
+            int width = Convert.ToInt32(Math.Round(Width * ((double)1 / 2 - (double)1 / 8)));
+            int heigth = Convert.ToInt32(Math.Round(Height * ((double)1 / 6 - (double)2 / 25)));
+            newButton.Location = new Point(x, y);
+            newButton.Size = new Size(width, heigth);
+            newButton.UseVisualStyleBackColor = true;
+        }
+
+    }
     public partial class CalculatorWindow : Form
     {
-        internal delegate double OperationMethod(double a, double b);
+        public static int PanelCounter = -1;
         internal static OperationMethod currentOperationMethod;
-        private static int PanelCounter = -1;
         public CalculatorWindow()
         {
             InitializeComponent();
@@ -23,15 +46,29 @@ namespace WindowsFormsApp1
         }
         private void CalculatorWindow_Load(object sender, EventArgs e)
         {
-            InitializeDefaultOperations();
+            InitializeOperations();
             InitializeButtons();
             Initialize_OperationPanel();
         }
 
-
+        internal static double ExecuteOperation(CalculatorWindow CalculatorWindow, OperationMethod Operation)
+        {
+            switch (CalculatorWindow.ValidateInput())
+            {
+                case true:
+                    break;
+                case false:
+                    MessageBox.Show("Введите число");
+                    return 0;
+            }
+            if (Operation != null) return Operation(Convert.ToDouble(CalculatorWindow.TextBox1.Text), Convert.ToDouble(CalculatorWindow.TextBox2.Text));
+            else
+                MessageBox.Show("Не выбрана операция");
+            return 0;
+        }
         private void Button1_Click(object sender, EventArgs e)
         {
-            Label1.Text = Convert.ToString(OperationButton.ExecuteOperation(this, currentOperationMethod));
+            Label1.Text = Convert.ToString(ExecuteOperation(this, currentOperationMethod));
 
         }
         protected bool ValidateInput()
@@ -42,7 +79,7 @@ namespace WindowsFormsApp1
             return false;
         }
        
-        private void AddOperation(string OperationName, OperationMethod operationMethod)
+        internal void AddOperation(string OperationName, OperationMethod operationMethod)
         {
             Panel1.Controls.Add(new OperationButton(OperationName, operationMethod));
         }
@@ -87,20 +124,24 @@ namespace WindowsFormsApp1
         {
             return a / b;
         }
+        internal void InitializeOperations()
+        {
+            InitializeDefaultOperations();
+            InitializeUserOperations();
+        }
         private void InitializeDefaultOperations()
         {
             AddOperation("+", Add);
             AddOperation("-", Substract);
             AddOperation("*", Multiply);
             AddOperation("/", Divide);
-            InitializeUserOperations();
 
         }
         private static double Sample(double a, double b)
         {
             return 0;
         }
-        private void InitializeUserOperations()
+        public void InitializeUserOperations()
         {
             AddOperation("sample00000", Sample); 
             AddOperation("sample00001", Sample);
@@ -109,14 +150,14 @@ namespace WindowsFormsApp1
               AddOperation("sample00004", Sample);
                AddOperation("sample00005", Sample);
              AddOperation("sample00006", Sample);
-         /*    AddOperation("sample00007", Sample);
+             AddOperation("sample00007", Sample);
           AddOperation("sample000", Sample); //  (+ РЕСАЙЗ, 4 столбца операций)
             AddOperation("sample00204", Sample);
             AddOperation("sample00105", Sample);
             AddOperation("sample00306", Sample);
             AddOperation("sample00407", Sample);
              AddOperation("sample00008", Sample);
-             AddOperation("sample00009", Sample);
+          /*   AddOperation("sample00009", Sample);
              AddOperation("sample00010", Sample);
              AddOperation("sample00011", Sample);
              AddOperation("sample00012", Sample); 
--- WindowsFormsApp1/OperationButton.cs
+++ WindowsFormsApp1/OperationButton.cs
@@ -11,61 +11,48 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
+    public class OperationButton : Button
+    {
+        private static Button PushedButton;
+        public string OperationName;
+        public byte Index { get; }
+        public static byte Number = 1;
+
+        static Dictionary<string, OperationMethod> OperationMap = new Dictionary<string, OperationMethod>();
+        internal OperationButton(string operationName, OperationMethod operationMethod)
+        {
+            this.Name = operationName;
+            this.Text = operationName;
+            this.Index = Number;
+            this.OperationName = operationName;
+            this.UseVisualStyleBackColor = true;
+            ((Button)this).Click += Click;
+            OperationMap.Add(operationName, operationMethod);
+            Number++;
+        }
+
+
+
+        private new void Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (PushedButton != null)
+            {
+                PushedButton.BackColor = btn.BackColor;
+            }
+            PushedButton = btn;
+            btn.BackColor = System.Drawing.Color.LightSteelBlue;
+            OperationMap.TryGetValue(OperationName, out CalculatorWindow.currentOperationMethod);
+        }
+
+
+     
+    }
+
     public partial class CalculatorWindow : Form
     {
 
-        public class OperationButton : Button
-        {
-           private static Button PushedButton;
-           public string OperationName;
-           public byte Index { get; }
-           public static byte Number = 1;
-                
-           static Dictionary<string, OperationMethod> OperationMap = new Dictionary<string, OperationMethod>();
-            internal OperationButton(string operationName, OperationMethod operationMethod)
-            {
-                this.Name = operationName;
-                this.Text = operationName;
-                this.Index = Number;
-                this.OperationName = operationName;
-                this.UseVisualStyleBackColor = true;
-                ((Button)this).Click += Click;
-                OperationMap.Add(operationName, operationMethod);
-                Number++;
-            }
-
-            
-
-            private new void Click(object sender, EventArgs e)
-            {
-                Button btn = sender as Button;
-                if (PushedButton != null)
-                {
-                    PushedButton.BackColor = btn.BackColor;
-                }
-                PushedButton = btn;
-                btn.BackColor = System.Drawing.Color.LightSteelBlue;
-                OperationMap.TryGetValue(OperationName, out currentOperationMethod);
-            }
-
-
-            internal static double ExecuteOperation(CalculatorWindow CalculatorWindow, OperationMethod Operation)
-            {
-                switch (CalculatorWindow.ValidateInput())
-                {
-                    case true:
-                        break;
-                    case false:
-                        MessageBox.Show("Введите число");
-                        return 0;
-                }
-                if (Operation != null) return Operation(Convert.ToDouble(CalculatorWindow.TextBox1.Text), Convert.ToDouble(CalculatorWindow.TextBox2.Text));
-                else
-                    MessageBox.Show("Не выбрана операция");
-                    return 0;
-            }
-        }
-
+   
     }
 
 }
